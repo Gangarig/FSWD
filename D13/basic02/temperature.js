4a4f8e55512efc3849a328cf31b46549952f120c
Gangarig <112808191+Gangarig@users.noreply.github.com>
2022-09-27 23:58:18 +0200

    123

--- D13/basic02/temperature.js
+++ D13/basic02/temperature.js
@@ -2,25 +2,25 @@ let temp=Math.floor(Math.random()*40-5);
 if (temp >30){
     document.getElementById("text1").innerHTML=`Current Temperature is = ${temp}`;
     document.getElementById("text2").innerHTML=`The weather is really hot`;
-    
+    weatherimg(1);
 } else if ( temp > 20) {
     document.getElementById("text1").innerHTML=`Current Temperature is = ${temp}`;
     document.getElementById("text2").innerHTML=`The weather is warm`;
-
+    weatherimg(2);
 }   else if ( temp > 10) {
     document.getElementById("text1").innerHTML=`Current Temperature is = ${temp}`;
     document.getElementById("text2").innerHTML=`The weather is Moderate`;
-
+    weatherimg(3);
 } else {
     document.getElementById("text1").innerHTML=`Current Temperature is = ${temp}`;
     document.getElementById("text2").innerHTML=`The weather is really cold`;
-
+    weatherimg(4);
 }
-function weatherimg(<img>) {
+function weatherimg(img) {
     var x = document.createElement("IMG");
-    x.setAttribute("src", "Veryhot.jpg");
+    x.setAttribute("src", `${img}.jpg`);
     x.setAttribute("width", "342");
     x.setAttribute("height", "870");
     document.body.appendChild(x);
   }
-  weatherimg();
+ 
