41a1f49a48ff1d12b9eb3e32274fa64e8d822a22
Gangarig <112808191+Gangarig@users.noreply.github.com>
2022-10-10 08:36:07 +0200

    234

---
+++ Day-20.Typescript-02/Prework/advanced/script.js
@@ -0,0 +1,77 @@
+"use strict";
+let vehiceldata = [];
+//main class
+class Vehicles {
+    constructor(name, brand) {
+        this.name = name;
+        this.brand = brand;
+        vehiceldata.push(this);
+    }
+    printInfo() {
+        return `Name-${this.name} <br> 
+                Brand-${this.brand}<br>`;
+    }
+}
+//child class
+class Truck extends Vehicles {
+    constructor(name, brand, model, kilometers, year_of_production, price, number_of_seats) {
+        super(name, brand);
+        this.model = model;
+        this.kilometers = kilometers;
+        this.year_of_production = year_of_production;
+        this.price = price;
+        this.number_of_seats = number_of_seats;
+    }
+    printInfo() {
+        return `
+                model-${this.model} <br>
+                kilometers-${this.kilometers}<br>
+                built in year- ${this.year_of_production}<br>
+                price- ${this.price}$ <br>
+                nubmer of seats- ${this.number_of_seats}`;
+    }
+}
+//child class
+class Bike extends Vehicles {
+    constructor(name, brand, model, kilometers, year_of_production, price, number_of_seats) {
+        super(name, brand);
+        this.model = model;
+        this.kilometers = kilometers;
+        this.year_of_production = year_of_production;
+        this.price = price;
+        this.number_of_seats = number_of_seats;
+    }
+    printInfo() {
+        return `
+        model-${this.model} <br>
+        kilometers-${this.kilometers}<br>
+        built in year- ${this.year_of_production}<br>
+        price- ${this.price}$<br>
+        nubmer of seats- ${this.number_of_seats}`;
+    }
+}
+let rav4 = new Vehicles("Rav4", "Toyota");
+let supra = new Vehicles("Supra", "Toyota");
+console.log(rav4.printInfo());
+console.log(supra.printInfo());
+let ducati = new Bike("Ducati", "Ducati", "00000", 10000, 2020, 25000, 1);
+let cybertruck = new Truck("Cybertruck", "Tesla", "00000", 100, 2022, 70000, 4);
+console.log(ducati.printInfo());
+console.log(cybertruck.printInfo());
+function truckfunction() {
+    document.getElementById("cybertruck").innerHTML = cybertruck.printInfo();
+}
+;
+function bikefunction() {
+    document.getElementById("ducati").innerHTML = ducati.printInfo();
+}
+;
+function price(value) {
+    if (value < 100) {
+        return `${Bike.price}`;
+    }
+    else {
+        return `${Bike.price / 2}`;
+    }
+}
+;
--- Day-20.Typescript-02/Prework/script.js
+++ Day-20.Typescript-02/Prework/script.js
@@ -1,3 +1,28 @@
 "use strict";
-let col1 = "Hello world!";
-console.log(col1);
+let Personinfo = [];
+class Person {
+    constructor(name, age, jobtitle) {
+        this.name = name;
+        this.age = age;
+        this.jobtitle = jobtitle;
+        Personinfo.push(this);
+    }
+    printinfo() {
+        return `Hello there, My name is ${this.name} and i am ${this.age} years old, and i am a ${this.jobtitle}`;
+    }
+}
+let Ganaa = new Person("Gangarg", 26, "waiter at AK");
+console.log(Ganaa.printinfo());
+// Basic 02
+class Personwithmoreinfo extends Person {
+    constructor(name, age, jobtitle, salary, place) {
+        super(name, age, jobtitle);
+        this.salary = salary;
+        this.joblocation = place;
+    }
+    printinfo() {
+        return `${super.printinfo()} and I get ${this.salary} every month, and i work in ${this.joblocation}.`;
+    }
+}
+let Ganaawithmoredata = new Personwithmoreinfo("Gangarig", 26, "waiter at AK", "secret amount", "Wien");
+console.log(Ganaawithmoredata.printinfo());
