986a4cffc35b2cbad30b8fe8136e326298fc3c19
Gangarig <112808191+Gangarig@users.noreply.github.com>
2022-10-10 10:16:35 +0200

    123

--- Day-21.Angular-01/myangularapplication/src/app/app.module.ts
+++ Day-21.Angular-01/myangularapplication/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
